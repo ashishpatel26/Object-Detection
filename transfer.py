0372f0add33badd93ea35ef1b593f22d1686979d
Kurtis Streutker <kurtisstreutker@gmail.com>
2018-03-30 18:32:09 -0400

    edit

--- transfer.py
+++ transfer.py
@@ -1,6 +1,3 @@
-from keras.preprocessing.image import ImageDataGenerator
-
-#-----------------------------------------
 import os
 import cv2
 import numpy as np
@@ -17,26 +14,22 @@ for folder in os.listdir("data/flower_photos"):
         img = cv2.imread("data/flower_photos/" + str(folder) + '/' + str(filename))
 
         # resize and append
-        X.append(cv2.resize(img, (img_width, img_width)))
+        X.append(cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), (img_width, img_width)))
         y.append(label_cnt)
 
     # new label for new folder
     label_cnt += 1
 
-#-----------------------------------------
-
 # show random image from set
 import matplotlib.pyplot as plt
 rand = np.random.randint(0, len(X))
-rgb = cv2.cvtColor(np.array(X[rand]),cv2.COLOR_BGR2RGB)
-imgplot = plt.imshow(rgb)
+imgplot = plt.imshow(X[rand])
 plt.show()
 
 # scale and cast to np array as float32
 X = (np.array(X) / 255).astype(np.float32)
 y = np.array(y)
 
-#-----------------------------------------
 from keras.utils import to_categorical
 
 # shuffle
@@ -56,8 +49,6 @@ y_test = to_categorical(y_test, label_cnt)
 
 print("train: {0} | test: {1}".format(X_train.shape[0], X_test.shape[0]))
 
-#-----------------------------------------
-
 from keras import applications
 from keras import optimizers
 from keras.models import Sequential
@@ -67,35 +58,35 @@ from keras.layers import Dropout, Flatten, Dense, Input, Conv2D, MaxPooling2D
 model = Sequential()
 
 # Block 1
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', input_shape=(img_width, img_width, 3)))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(img_width, img_width, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # Block 2
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # Block 3
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # Block 4
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # Block 5
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # load pretrained weights
 model.load_weights('models/vgg19_weights.h5')
@@ -114,12 +105,22 @@ model.add(Dense(label_cnt, activation="softmax"))
 # compiling the model
 model.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
 
-#-----------------------------------------
+from keras.preprocessing.image import ImageDataGenerator
+
+# data augmentation for training
+datagen = ImageDataGenerator(width_shift_range=0.2, 
+                             height_shift_range=0.2,  
+                             horizontal_flip=True)
+
+datagen.fit(X_train)
 
 # Train the model 
-model.fit(x=X_train, y=y_train,
-                batch_size=16,
-                epochs = 10,
-                shuffle=True)
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=16),
+                    epochs = 10,
+                    shuffle=True)
+
 
 print('Test accuracy: {0:.3}%'.format(model.evaluate(x=X_test, y=y_test, batch_size=16)[1] * 100))
+
+# save model
+model.save('models/vgg_retrained.h5')
